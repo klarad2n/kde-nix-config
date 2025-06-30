be5a2ebedef2ca906302571f2a807ee30d49eef6
Klara d2n <c.berger89@outlook.de>
2025-06-30 17:17:40 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -34,15 +34,15 @@
   i18n.defaultLocale = "de_AT.UTF-8";
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "de_AT.UTF-8";
-    LC_IDENTIFICATION = "de_AT.UTF-8";
-    LC_MEASUREMENT = "de_AT.UTF-8";
-    LC_MONETARY = "de_AT.UTF-8";
-    LC_NAME = "de_AT.UTF-8";
-    LC_NUMERIC = "de_AT.UTF-8";
-    LC_PAPER = "de_AT.UTF-8";
-    LC_TELEPHONE = "de_AT.UTF-8";
-    LC_TIME = "de_AT.UTF-8";
+  LC_ADDRESS = "de_AT.UTF-8";
+  LC_IDENTIFICATION = "de_AT.UTF-8";
+  LC_MEASUREMENT = "de_AT.UTF-8";
+  LC_MONETARY = "de_AT.UTF-8";
+  LC_NAME = "de_AT.UTF-8";
+  LC_NUMERIC = "de_AT.UTF-8";
+  LC_PAPER = "de_AT.UTF-8";
+  LC_TELEPHONE = "de_AT.UTF-8";
+  LC_TIME = "de_AT.UTF-8";
   };
 
   # Enable the X11 windowing system.
@@ -55,8 +55,8 @@
 
   # Configure keymap in X11
   services.xserver.xkb = {
-    layout = "at";
-    variant = "nodeadkeys";
+  layout = "at";
+  variant = "nodeadkeys";
   };
 
   # Enable CUPS to print documents.
@@ -66,16 +66,16 @@
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+  enable = true;
+  alsa.enable = true;
+  alsa.support32Bit = true;
+  pulse.enable = true;
+  # If you want to use JACK applications, uncomment this
+  #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+  # use the example session manager (no others are packaged yet so this is enabled by default,
+  # no need to redefine it in your config for now)
+  #media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
@@ -83,12 +83,12 @@
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.omega = {
-    isNormalUser = true;
-    description = "omega";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    kdePackages.kate
-    #  thunderbird
+  isNormalUser = true;
+  description = "omega";
+  extraGroups = [ "networkmanager" "wheel" ];
+  packages = with pkgs; [
+  kdePackages.kate
+  #  thunderbird
     ];
   };
 
